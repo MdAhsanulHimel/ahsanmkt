876ff2a6712655246247c06c8b699fe831681bd4
Md Ahsanul Islam <67047779+MdAhsanulHimel@users.noreply.github.com>
2023-12-27 00:58:05 +0600

    Added description of the dataset.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' The Popular mtcars Dataset
+#'
+#' This is the mtcars dataset. This is added just as a demo.
+#'
+#' @format ## `mtcars`
+#' A data frame with 32 rows and 11 columns:
+#' \describe{
+#'   \item{mpg}{Miles/(US) gallon}
+#'   \item{cyl}{Number of cylinders}
+#'   \item{disp}{Displacement (cu.in.)}
+#'   ...
+#' }
+#' @source  Henderson and Velleman (1981), Building multiple regression models interactively. Biometrics, 37, 391–411.
+"mtcars"
