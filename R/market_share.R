9dbdf95f3804175d74870557bae4f61ff969cad6
Md Ahsanul Islam <ahsanulislam10@gmail.com>
2024-01-03 13:27:26 +0600

    added new market share function

--- R/market_share.R
+++ R/market_share.R
@@ -111,3 +111,91 @@ market_share <- function(data,
 }
 
 
+
+
+
+
+
+
+
+#' Market Share Calculation - Single Fact
+#'
+#' @param data
+#' @param market
+#' @param product_level
+#' @param base_level
+#' @param fact
+#' @param fact_amount
+#' @param fact_share
+#' @param decimals
+#' @param progress_full
+#'
+#' @return
+#' @export
+#'
+#' @examples
+market_share_single <- function(data,
+                                market = "MARKET",
+                                product_level = "PROD_LEVEL",
+                                base_level = "1_CATEGORY_TOTAL",
+                                fact = "ATTRIBUTE",
+                                fact_amount = "Sales Offtake Value (mil)",
+                                fact_share = "Market Share (Value)",
+                                decimals = 7,
+                                progress_full = FALSE){
+
+  stopifnot("Attribute column not found" = fact %in% colnames(data),
+            "Product level column not found" = product_level %in% colnames(data),
+            "Market column not found" = market %in% colnames(data))
+
+  stopifnot("Passed argument in base_level not found in product level" =
+              base_level %in% unique(data[[product_level]]),
+            "Passed argument in fact_amount not found in attributes" =
+              fact_amount %in% unique(data[[fact]]),
+            "Passed argument in fact_share not found in attributes" =
+              fact_share %in% unique(data[[fact]]))
+
+
+  data2 <- data %>%
+    filter(!(data[[fact]] %in% fact_share)) %>%
+    mutate(across(.cols = names(data)[1:which(names(data)==fact)], .fns = factor))
+
+  data_share <- data2[1,]
+
+  i <- 0
+  for (m in unique(data2[[market]])) {
+    i <- i+1
+    message("\n",i," Calculating Market Share in ", m)
+
+    deno_val <- data2 %>%   # deno means Denominator
+      dplyr::filter(!!dplyr::sym(market) == m,
+                    !!dplyr::sym(product_level) == base_level,
+                    !!dplyr::sym(fact) == val)
+
+    for (p in unique(data[[product_level]])) {
+      if(progress_full == TRUE) {message("---Product Level: ", p)}
+
+      # Calculating Market Share Value
+      nume_val <- data2 %>%  # nume means Numerator
+        filter(!!dplyr::sym(market) == m,
+               !!dplyr::sym(product_level) == p,
+               !!dplyr::sym(fact) == val)
+
+      deno_val_extended <- deno_val[rep(1, each = nrow(nume_val)),]
+
+      share <- data.frame(nume_val[, sapply(nume_val, is.factor)],
+                              nume_val[, sapply(nume_val, is.numeric)] / deno_val_extended[, sapply(nume_val, is.numeric)],
+                              check.names = F) %>%
+        mutate(
+          across(where(is.numeric), ~ round(., decimals)),
+          !!fact := case_when(!!dplyr::sym(fact) == val ~ val_share)
+        )
+
+      data_share <- rbind(data_share, share)
+
+    }
+  }
+
+  data_share <- data_share[-1,]
+  return(rbind(data2, data_share))
+}
